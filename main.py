275520d99d5b57ca8f84f3fef4ad13e52a70a22f
grepoz <grzegorz.pozorski@gmail.com>
2023-06-04 19:08:04 +0200

    add months count extraction & adjust progress bar update

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from tqdm import tqdm
 
 from data_manager import create_data
 from genetics_algorithm import GeneticAlgorithm
-from utils import get_static_wallet_value
+from utils import get_static_wallet_value, months_between_dates
 
 # data
 tickers = ['^IXIC', 'GLD', 'USDPLN=X']
-start_date = '2021-01-01'
-end_date = '2021-12-31'
+start_date = '2020-06-03'
+end_date = '2023-06-03'
 ASSETS_NAMES = ['NASDAQ', 'Gold', 'USD/PLN', 'Bonds', 'Cash']
 
 # algorithm config
@@ -15,13 +15,13 @@ POPULATION_SIZE = 100
 GENES_COUNT = 5
 CROSSOVER_PROBABILITY = 0.65
 MUTATION_PROBABILITY = 0.2
-GENERATION_COUNT = 10000
+GENERATION_COUNT = 200
 
 # finance config
 INITIAL_CASH = 1000
 MONTHLY_CONTRIBUTION = 100
-CONTRIBUTION_PERIODS = 12  # could be calc
-EXPECTED_REWARD_PERCENTAGE = 15
+CONTRIBUTION_PERIODS = months_between_dates(start_date, end_date)
+EXPECTED_REWARD_PERCENTAGE = 20
 
 if __name__ == '__main__':
 
@@ -43,14 +43,14 @@ if __name__ == '__main__':
             best_index = ga.selection(population)
             highest_gain = ga.fitness(population[best_index])
 
+            pbar.set_postfix({'highest gain': highest_gain}, refresh=True)
+            pbar.update(1)
+
             if highest_gain >= expected_reward_in_cash:
                 found_expected_best = True
                 break
 
             population = ga.crossover(population, best_index, CROSSOVER_PROBABILITY, MUTATION_PROBABILITY)
-
-            pbar.set_postfix({'highest gain': highest_gain}, refresh=True)
-            pbar.update(1)
 
     print('=== Founded expected best ===' if found_expected_best else '=== Didn\'t find expected best ===')
     ga.print_best(population[best_index])
--- utils.py
+++ utils.py
@@ -1,2 +1,13 @@
+from datetime import datetime
+
+
 def get_static_wallet_value(initial_cash, monthly_contribution, contribution_periods):
     return initial_cash + monthly_contribution * contribution_periods
+
+
+def months_between_dates(start_date: str, end_date: str) -> int:
+    start = datetime.strptime(start_date, '%Y-%m-%d')
+    end = datetime.strptime(end_date, '%Y-%m-%d')
+
+    return (end.year - start.year) * 12 + end.month - start.month
+
